1c1d4baf2761e3f7844ae82801121d498212ca69
Kasia <efefre@icloud.com>
2019-10-05 20:42:21 +0200

    Delete validation (cancel_date > end_date)

--- ticketWeb_root/tickets/utils.py
+++ ticketWeb_root/tickets/utils.py
@@ -22,8 +22,6 @@ class Ticket:
         if self.handling_fee > 50:
             self.handling_fee = 50
 
-        if self.cancel_date > self.end_date:
-            return 'Wprowadzono błędną datę. Nie można zwrócić biletu po terminie ważności'
         self.cancled_days = self.end_date - self.cancel_date
         self.money_back = round(float((self.ticket_price - self.handling_fee)/self.day * int(self.cancled_days.days)),2)
 
