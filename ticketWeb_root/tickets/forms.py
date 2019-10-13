244b5e433119472f6a1b0d283b640b423e439be3
Kasia <efefre@icloud.com>
2019-10-13 10:27:19 +0200

    Add form validation: period between start_date and stop_date

--- ticketWeb_root/tickets/forms.py
+++ ticketWeb_root/tickets/forms.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django import forms
 
 
@@ -38,6 +40,7 @@ class TicketForm(forms.Form):
         start_date = cleaned_data.get('start_date')
         stop_date = cleaned_data.get('stop_date')
         cancel_date = cleaned_data.get('cancel_date')
+        period = int(cleaned_data.get('period'))
 
         if start_date == None and stop_date == None:
             msg ='Musisz podać datę aktywacji bilet albo do kiedy bilet jest ważny'
@@ -47,6 +50,15 @@ class TicketForm(forms.Form):
             if start_date >= stop_date:
                 self.add_error('start_date', 'Data aktywacji biletu nie może być późniejsza niż data końca ważności biletu.')
                 self.add_error('stop_date', 'Data końca ważności biletu nie może być wcześniejsza niż data aktywacji biletu.')
+
+            start_date_including_period = stop_date - datetime.timedelta(days=period) + datetime.timedelta(days=1)
+            stop_date_including_period = start_date + datetime.timedelta(days=period) - datetime.timedelta(days=1)
+
+            if start_date != start_date_including_period or stop_date != stop_date_including_period:
+                self.add_error('start_date',
+                               'Między datą aktywacji biletu, a datą ważności biletu nie ma {} dni.'.format(period))
+                self.add_error('stop_date',
+                               'Między datą aktywacji biletu, a datą ważności biletu nie ma {} dni.'.format(period))
 
         if start_date != None:
             if start_date >= cancel_date:
